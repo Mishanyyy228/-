1aa039c7c23c3bf40e25c94f5953e19cb44b861d
Mishanyyy228 <mananev13@gmail.com>
2024-10-06 18:32:09 +0300

    Add files via upload

---
+++ Разработка ЛБ1/MainWindow - Копировать.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Разработка_ЛБ1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow___Копировать.xaml
+    /// </summary>
+    public partial class MainWindow___Копировать : Window
+    {
+        public MainWindow___Копировать()
+        {
+            InitializeComponent();
+        }
+    }
+}
